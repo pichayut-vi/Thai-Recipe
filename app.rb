f17c7c555d53f7579adb2d8d19437a664e1db6d2
Chayanont Vongsiripat <stampz74@gmail.com>
2013-04-10 14:57:04 +0700

    update app.rb

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ require 'mysql2'
 # this takes a hash of options, almost all of which map directly
 # to the familiar database.yml in rails
 # See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/MysqlAdapter.html
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "root", :encoding => "utf8",
-							 :database => "treasurehunter", :socket => "/Applications/MAMP/tmp/mysql/mysql.sock")
+
+client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "root", :encoding => "utf8", :database => "treasurehunter", :socket => "/Applications/MAMP/tmp/mysql/mysql.sock")
 
 get '/' do
 	"Hello from Sinatra on Heroku!"
