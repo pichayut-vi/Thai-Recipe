6819b239aef40edfb689b0b84108359916803318
Chayanont Vongsiripat <stampz74@gmail.com>
2013-04-10 12:52:05 +0700

    update app.rb connected database success !

--- app.rb
+++ app.rb
@@ -1,30 +1,12 @@
 require 'rubygems'
 require 'sinatra'
+require 'mysql2'
 
-Mysql2::Client.new(
-  :host,
-  :username,
-  :password,
-  :port,
-  :database,
-  :socket = '/path/to/mysql.sock',
-  :flags = REMEMBER_OPTIONS | LONG_PASSWORD | LONG_FLAG | TRANSACTIONS | PROTOCOL_41 | SECURE_CONNECTION | MULTI_STATEMENTS,
-  :encoding = 'utf8',
-  :read_timeout = seconds,
-  :write_timeout = seconds,
-  :connect_timeout = seconds,
-  :reconnect = true/false,
-  :local_infile = true/false,
-  )
-
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :flags => Mysql2::Client::MULTI_STATEMENTS )
-result = client.query( 'CALL sp_customer_list( 25, 10 )')
-# result now contains the first result set
-while ( client.next_result)
-    result = client.store_result
-    # result now contains the next result set
-end
-
+# this takes a hash of options, almost all of which map directly
+# to the familiar database.yml in rails
+# See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/MysqlAdapter.html
+client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "root", :encoding => "utf8",
+							 :database => "treasurehunter", :socket => "/Applications/MAMP/tmp/mysql/mysql.sock")
 
 get '/' do
 	"Hello from Sinatra on Heroku!"
