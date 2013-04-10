214ff2f5a2b9f82aa8ffbe6341ab4d6629839d7a
Chayanont Vongsiripat <stampz74@gmail.com>
2013-04-10 11:16:34 +0700

    Try to connect mysql2

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'rubygems'
 require 'sinatra'
 
+client = Mysql2::Client.new(:host => "localhost", :username => "root", :flags => Mysql2::Client::MULTI_STATEMENTS )
+result = client.query( 'CALL sp_customer_list( 25, 10 )')
+# result now contains the first result set
+while ( client.next_result)
+    result = client.store_result
+    # result now contains the next result set
+end
+
+
 get '/' do
 	"Hello from Sinatra on Heroku!"
 end
