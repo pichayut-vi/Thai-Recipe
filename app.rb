2a1baa58c9b51e4762bf1b8d90f9c570c412e12c
Chayanont Vongsiripat <stampz74@gmail.com>
2013-04-10 11:17:34 +0700

    update mysql2 connection

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'rubygems'
 require 'sinatra'
 
+Mysql2::Client.new(
+  :host,
+  :username,
+  :password,
+  :port,
+  :database,
+  :socket = '/path/to/mysql.sock',
+  :flags = REMEMBER_OPTIONS | LONG_PASSWORD | LONG_FLAG | TRANSACTIONS | PROTOCOL_41 | SECURE_CONNECTION | MULTI_STATEMENTS,
+  :encoding = 'utf8',
+  :read_timeout = seconds,
+  :write_timeout = seconds,
+  :connect_timeout = seconds,
+  :reconnect = true/false,
+  :local_infile = true/false,
+  )
+
 client = Mysql2::Client.new(:host => "localhost", :username => "root", :flags => Mysql2::Client::MULTI_STATEMENTS )
 result = client.query( 'CALL sp_customer_list( 25, 10 )')
 # result now contains the first result set
