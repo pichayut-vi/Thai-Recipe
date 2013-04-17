2f493bbf8102b471234b07e893c416c1a25a34d6
Chayanont Vongsiripat <stampz74@gmail.com>
2013-04-17 11:19:59 +0700

    commit something

--- app.rb
+++ app.rb
@@ -3,10 +3,6 @@ require 'sinatra'
 require 'mysql2'
 require 'json'
 
-# this takes a hash of options, almost all of which map directly
-# to the familiar database.yml in rails
-# See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/MysqlAdapter.html
-
 #client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "root", :encoding => "utf8",
 #						 :database => "treasurehunter", :socket => "C:/xampp/mysql/mysql.sock")
 
@@ -20,10 +16,11 @@ end
 get '/map' do
 	res = Array.new
 	array = [:id, :username, :session, :time]
-	result = client.query("SELECT * FROM loginsessions",:as => :array) 
+	result = client.query("SELECT * FROM loginsessions",:as => :array) #object as array
 	result.each do | row |
 		res.push(Hash[array.zip(row)])
 	end
 	return res.to_json
 end
+
 
